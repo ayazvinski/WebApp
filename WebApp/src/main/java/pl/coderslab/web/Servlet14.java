4157db1ddf7d317a7155bf6a33c2ded912fb263b
ayazvinski <a.yazvinski@limitless.pl>
2023-10-07 12:56:06 +0200

    nowy projekt Maven

---
+++ WebApp/src/main/java/pl/coderslab/web/Servlet14.java
@@ -0,0 +1,20 @@
+package pl.coderslab.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Servlet14", urlPatterns = "/servlet14")
+public class Servlet14 extends HttpServlet {
+
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRemoteAddr();
+        resp.getWriter().println(req.getRemoteAddr());
+
+        }
+
+    }
