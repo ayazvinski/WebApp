1817b8ea7df68f89e512a08971caeec061368647
ayazvinski <a.yazvinski@limitless.pl>
2023-10-10 17:37:57 +0200

    day3

--- WebApp/src/main/java/pl/coderslab/form/Form1.java
+++ WebApp/src/main/java/pl/coderslab/form/Form1.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet ("/post1")
+@WebServlet (name = "Form1", urlPatterns = "/post1")
 
 public class Form1 extends HttpServlet {
     @Override
--- WebApp/src/main/java/pl/coderslab/form/Form2.java
+++ WebApp/src/main/java/pl/coderslab/form/Form2.java
@@ -1,6 +1,35 @@
 package pl.coderslab.form;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet ("/post2")
 
 public class Form2 extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String[] badWords = {"cholera", "idiota","frajer"};
+
+        String inputMessage = req.getParameter("userMessage");
+        String userAcknowledge = req.getParameter("userAcknowledge");
+
+        if(userAcknowledge != null){
+            resp.getWriter().println(inputMessage);
+        } else {
+            for(String notAllowed : badWords){
+                final String stars = "*".repeat(notAllowed.length());
+                inputMessage = inputMessage.replaceAll(notAllowed, stars);
+            }
+            resp.getWriter().println(inputMessage);
+        }
+
+
+
+
+
+    }
 }
