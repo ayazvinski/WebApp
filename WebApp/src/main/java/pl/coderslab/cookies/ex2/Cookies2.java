44b0a1027977018860b650d1b861c4fbbb3131bf
ayazvinski <a.yazvinski@limitless.pl>
2023-10-10 19:17:45 +0200

    day3

--- WebApp/src/main/java/pl/coderslab/cookies/ex2/Cookies2.java
+++ WebApp/src/main/java/pl/coderslab/cookies/ex2/Cookies2.java
@@ -1,4 +1,25 @@
 package pl.coderslab.cookies.ex2;
 
-public class Cookies2 {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet ("/addToCookies")
+
+public class Cookies2 extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String cookieKey = req.getParameter("key");
+        String cookieValue = req.getParameter("value");
+
+        Cookie cookie = new Cookie(cookieKey, cookieValue);
+        resp.addCookie(cookie);
+        resp.getWriter().println("Ciasteczko " + cookieKey + " : " + cookieValue + " zostało dodane!");
+
+    }
 }
